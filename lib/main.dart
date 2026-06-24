a78e695d46b6d268a5d4a833ca7938febf0f715e
zufa1984 <abdulqodirkhamidov@gmail.com>
2026-06-24 15:57:27 +0500

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Coinvert',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: MainScreen(),
     );
